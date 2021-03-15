c92e1b6ffcd9250f2a9984be7967d2c3d18e089c
mmiiles <milesconrad@pm.me>
2021-03-15 16:11:02 -0400

    fixed variable names

--- control.py
+++ control.py
@@ -1,38 +1,38 @@
 import discord
 from discord.ext import commands
-import pydirectinput as con
-from pydirectinput import KEYBOARD_MAPPING
+import pydirectinput as control
+from pydirectinput import KEYBOARD_MAPPING as KEYS
 from pynput.keyboard import Controller
-from time import sleep as s
+from time import sleep
 token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 bot = commands.Bot(command_prefix='/')
-k = Controller()
+keyboard = Controller()
 
 print("Running...")
 
 @bot.event
 async def on_message(msg):
     if str(msg.channel) == 'pc-control':
-        st = msg.content.lower()
-        if st in KEYBOARD_MAPPING:
-            con.keyDown(st)
-            s(0.4)
-            con.keyUp(st)
+        content = msg.content.lower()
+        if content in KEYS:
+            control.keyDown(content)
+            sleep(0.4)
+            control.keyUp(content)
         else:
-            if st == 'mup':
-                con.move(0, -200)
-            if st == 'mdown':
-                con.move(0, 200)
-            if st == 'mleft':
-                con.move(-200, 0)
-            if st == 'mright':
-                con.move(200, 0)
-            if st == 'm1':
-                con.click(button = 'left')
-            if st == 'm2':
-                con.click(button = 'right')
-            if st.startswith('say'):
-                k.type(st[4:])
+            if content == 'mup':
+                control.move(0, -200)
+            if content == 'mdown':
+                control.move(0, 200)
+            if content == 'mleft':
+                control.move(-200, 0)
+            if content == 'mright':
+                control.move(200, 0)
+            if content == 'm1':
+                control.click(button = 'left')
+            if content == 'm2':
+                control.click(button = 'right')
+            if content.contentartswith('say'):
+                keyboard.type(content[4:])
         await msg.delete()
 
 bot.run(token)
