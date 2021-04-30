66fe8caa94721bc3508d0e05f72e76087bd75f6d
mmiiles <milesconrad@pm.me>
2021-04-30 15:24:12 -0400

    added comments

--- control.py
+++ control.py
@@ -4,7 +4,7 @@ import pydirectinput as control
 from pydirectinput import KEYBOARD_MAPPING as KEYS
 from pynput.keyboard import Controller
 from time import sleep
-token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+token = 'xxxxxxxxxxxxxxxxxxxxxxxx'
 bot = commands.Bot(command_prefix='/')
 keyboard = Controller()
 
@@ -14,10 +14,14 @@ print("Running...")
 async def on_message(msg):
     if str(msg.channel) == 'pc-control':
         content = msg.content.lower()
+
+        #checks if selected key is a key object
         if content in KEYS:
             control.keyDown(content)
             sleep(0.4)
             control.keyUp(content)
+        
+        #if selected key is not a key object, check if the key is a mouse movement
         else:
             if content == 'mup':
                 control.move(0, -200)
@@ -33,6 +37,7 @@ async def on_message(msg):
                 control.click(button = 'right')
             if content.startswith('say'):
                 keyboard.type(content[4:])
+                
         await msg.delete()
 
 bot.run(token)
